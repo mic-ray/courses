7b9eb51b5c91fdae51319d86682b209fcea5c65d
Raykhlin <michael.a.raykhlin@capgemini.com>
2020-07-19 17:45:40 +0200

    Finished basic random quote machine
    JavaScript:
    - Ajax is used to fetch a json object containing the quotes from a gist
    - Added function to initialize quotes on window load, which fetches the
    quotes, and sets an initial quote
    - setQuote function sets a random quote to display and adds a target
    link to the tweet-quote button
    - randomQuote function just returns a random quote from the quotes object
    
    (S)CSS:
    - Added some general styling to the page
    
    HTML:
    - Added a "creator" row, containing info about creator of page

--- fcc/FE Libraries Course/Random Quote Machine/index.js
+++ fcc/FE Libraries Course/Random Quote Machine/index.js
@@ -1,4 +1,46 @@
+// Array holding the quote objects
+var quotes = [];
+
 $(function () {
-    $('#new-quote').click(() => console.log('New Quote requested'));
-    $('#tweet-quote').click(() => console.log('Tweet requested'));
+    // Initialize quote display
+    initQuote();
+    // Add event listener to new quote button
+    $('#new-quote').click(() => setQuote());
 });
+
+/**
+ * Initializes the quote display
+ */
+const initQuote = () => {
+    // Get quotes from the provided gist
+    $.ajax({
+        url:
+            'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
+        success: (res) => {
+            quotes = JSON.parse(res).quotes;
+            setQuote();
+        },
+    });
+};
+
+/**
+ * Set a random quote to display
+ */
+const setQuote = () => {
+    // Get a random quote
+    let quote = getRandomQuote();
+    // Set quote text and author
+    $('#text').text('"' + quote.quote + '"');
+    $('#author').text('- ' + quote.author);
+    // Set link on tweet quote button to share quote
+    $('#tweet-quote').attr(
+        'href',
+        'https://twitter.com/intent/tweet?text=' +
+            encodeURIComponent('"' + quote.quote + '" -' + quote.author)
+    );
+};
+
+/**
+ * Returns a random quote
+ */
+const getRandomQuote = () => quotes[Math.floor(Math.random() * quotes.length)];
