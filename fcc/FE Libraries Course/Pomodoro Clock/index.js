590e3d8dc64eb1589f3b029d568357733b2ba522
Raykhlin <michael.a.raykhlin@capgemini.com>
2020-07-22 21:03:12 +0200

    Finished Pomodoro Clock project
    Audio:
    - Added required audio element to HTML
    - Is played from the JS code, when timer reaches 00:00
    - Is stopped and reset, when reset button is clicked
    
    Session/Break switch:
    - Added state variable, that holds info about wheter a session or a break
    is currently running
    - When the timer reaches 00:00 the timer label is switched between
    Session and Break, the time length value is also switched to the
    corresponding one, the timer is reset and the beep audio is played
    
    Timer function:
    - Switched the display and decremting of the timer value to provide
    the required functionality (so that the starting timer length is
    displayed first, instead directly decrementing it on timer start)

--- fcc/FE Libraries Course/Pomodoro Clock/index.js
+++ fcc/FE Libraries Course/Pomodoro Clock/index.js
@@ -1,15 +1,28 @@
+// Length of break
 var breakLength = 5;
+// Length of session
 var sessionLength = 25;
+// Current left time
 var time = sessionLength * 60;
+// Flag, that displays, whether timer
+// is paused at the moment or not
 var paused = true;
+// State displaying whether a session
+// or a break is running
+var state = 'Session';
 
+// On window load
 $(function () {
+    // Initiliaze the timers
     initTimer();
-    // Add event listener to new quote button
+    // Add event listeners to
+    // session/break in/decrement
     $('#break-increment').click(() => {
         if (paused) {
-            breakLength++;
-            initTimer();
+            if (breakLength < 60) {
+                breakLength++;
+                initTimer();
+            }
         }
     });
     $('#break-decrement').click(() => {
@@ -38,38 +51,98 @@ $(function () {
             }
         }
     });
+    // Switch paused flag and update
+    // timer when start stop is clicked
     $('#start_stop').click(() => {
         paused = !paused;
         timer();
     });
+    // Add event listener to reset timer
+    // when reset button is pressed
     $('#reset').click(() => resetTimer());
 });
 
+/**
+ * Initializes the timer and labels
+ */
 const initTimer = () => {
     $('#break-length').text(breakLength);
     $('#session-length').text(sessionLength);
     $('#time-left').text(convertTime(time));
+    $('#timer-label').text(state);
 };
 
+/**
+ * Handles timer functionality
+ */
 const timer = () => {
+    /**
+     * Inner function, that ticks down
+     * the timer
+     */
     const updateTimer = () => {
-        if (!paused) {
-            time--;
+        // If the timer ran out
+        if (time < 0) {
+            // Switch timer function
+            timeSwitch();
+            // If timer is not paused
+        } else if (!paused) {
+            // Show current time
             $('#time-left').text(convertTime(time));
+            // Decrease time
+            time--;
+            // Call inner function after a second again
             setTimeout(updateTimer, 1000);
         }
     };
     updateTimer();
 };
 
+/**
+ * Handles session/break switch of timer
+ */
+const timeSwitch = () => {
+    // Play audio sound
+    document.getElementById('beep').play();
+    // Pause the timer
+    paused = true;
+    // Switch the state
+    // and assign corresponding time
+    if (state === 'Session') {
+        state = 'Break';
+        time = breakLength * 60;
+    } else {
+        state = 'Session';
+        time = sessionLength * 60;
+    }
+    // Set label to current state
+    $('#timer-label').text(state);
+    // Unpause timer again
+    paused = false;
+    timer();
+};
+
+/**
+ * Resets the whole timer
+ */
 const resetTimer = () => {
+    // Stop and reset beep sound
+    document.getElementById('beep').pause();
+    document.getElementById('beep').currentTime = 0;
+    // Set all lengths and labels
+    // back to default values
     breakLength = 5;
     sessionLength = 25;
     time = sessionLength * 60;
+    state = 'Session';
     paused = true;
     initTimer();
 };
 
+/**
+ * Converts given [time] value to a mm:ss string
+ * @param {*} time Time value (in seconds)
+ */
 const convertTime = (time) => {
     return (
         Math.floor(time / 60)
