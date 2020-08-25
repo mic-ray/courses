356b438e8591dabf2ec8cfaa648766e6f4065235
Raykhlin <michael.a.raykhlin@capgemini.com>
2020-08-25 12:06:23 +0200

    Finished Markdown Previewer
    React:
    - Added the required markdown elements to the initial text
    - Adjusted some Marked.js options to pass requirements
    
    (S)CSS:
    - Adjusted styling of preview area for better display of markdown elements

--- fcc/FE Libraries Course/Markdown Previewer/index.js
+++ fcc/FE Libraries Course/Markdown Previewer/index.js
@@ -38,7 +38,37 @@ class App extends React.Component {
     }
 }
 
-const initialText = 'Some initial text';
+// Required initial text
+const initialText = `This is some markdown to pe previewed.
+# This is a heading of level 1!
+## This is a heading of level 2!
+
+This is an [example link](https://google.com)!
+
+\`<div>This is some inline code!<div>\`
+
+\`\`\`html
+    <html>
+        <body>
+            This is some multi-line code!
+        </bod>
+    </html>
+\`\`\`
+
+1. This is a list item.
+2. This one too!
+
+> This is a blockquote!
+
+![This is an image](https://en.wikipedia.org/wiki/Google_logo#/media/File:Google_2015_logo.svg)
+
+Finally some **bolded text!**
+`;
+
+// Set Marked.js options to pass requirements
+marked.setOptions({
+    breaks: true,
+});
 
 // Render main App component on #app element
 ReactDOM.render(<App />, document.getElementById('app'));
